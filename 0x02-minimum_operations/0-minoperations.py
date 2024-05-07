3f609183d9e392392138e603f3c64a59fdc5141d
GirumZ <girum.zebe@gmail.com>
2024-05-07 18:38:15 +0300

    [Create]: A python function to find the min number of operations needed to have n number on 'H's

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+A python function that calculate the minimum amount of operation
+needed to get n number of 'H's in a file
+"""
+
+
+def minOperations(n):
+    """A python function that calculate the minimum amount of operation
+    needed to get n number of 'H's in a file
+    Args:
+        n(int): the number of 'H's needed
+    Returns:
+        The total operations needed
+    """
+
+    dp = [0] * (n + 1)
+
+    for i in range(2, n + 1):
+        dp[i] = float('inf')
+        for j in range(1, i):
+            if i % j == 0:
+                dp[i] = min(dp[i], dp[j] + i // j)
+
+    return dp[n]
