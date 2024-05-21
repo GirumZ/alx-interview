089ff2fb453c018b488a4d1dd14f5e8fbdc3a646
GirumZ <girum.zebe@gmail.com>
2024-05-21 11:14:57 +0300

    [Create]: A utf-8 validation method

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" A python module for utf-8 validation"""
+
+def validUTF8(data):
+    """ method for utf-8 validation"""
+
+    one_byte_mask = 0b10000000
+    two_byte_mask = 0b11100000
+    three_byte_mask = 0b11110000
+    four_byte_mask = 0b11111000
+    continuation_mask = 0b11000000
+
+    i = 0
+    num_bytes = 0
+    while i < len(data):
+        byte = data[i]
+
+        if (byte & one_byte_mask) == 0:
+            num_bytes = 1
+        elif (byte & two_byte_mask) == 0b11000000:
+            num_bytes = 2
+        elif (byte & three_byte_mask) == 0b11100000:
+            num_bytes = 3
+        elif (byte & four_byte_mask) == 0b11110000:
+            num_bytes = 4
+        else:
+            return False
+
+        if i + num_bytes > len(data):
+            return False
+        
+        for j in range(1, num_bytes):
+            if (data[i + j] & continuation_mask) != 0b10000000:
+                return False
+
+        i += num_bytes
+
+    return True    
