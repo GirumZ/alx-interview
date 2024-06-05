0b2068311bbd818eeefcfd0a46b80475a3ac2b08
GirumZ <girum.zebe@gmail.com>
2024-06-05 23:37:53 +0300

    [Bug fix]: 2 spaces for tab

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,18 +4,18 @@ const util = require('util');
 const request = util.promisify(require('request'));
 const filmID = process.argv[2];
 
-async function getSwCharacters(filmID) {
-	const url = 'https://swapi-api.alx-tools.com/api/films/' + filmID;
-	let response = await (await request(url)).body;
-	response = JSON.parse(response);
-	const characters = response.characters;
+async function getSwCharacters (filmID) {
+  const url = 'https://swapi-api.alx-tools.com/api/films/' + filmID;
+  let response = await (await request(url)).body;
+  response = JSON.parse(response);
+  const characters = response.characters;
 
-	for (let i = 0; i < characters.length; i++) {
-		const newUrl = characters[i];
-		let character = await (await request(newUrl)).body;
-		character = JSON.parse(character);
-		console.log(character.name);
-	}
+  for (let i = 0; i < characters.length; i++) {
+    const newUrl = characters[i];
+    let character = await (await request(newUrl)).body;
+    character = JSON.parse(character);
+    console.log(character.name);
+  }
 }
 
 getSwCharacters(filmID);
