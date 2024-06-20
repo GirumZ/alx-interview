2773bc348b7f5b304674a65176db4b0db4be7f32
GirumZ <girum.zebe@gmail.com>
2024-06-20 09:10:04 +0300

    [Update]: Used greedy algorithm for efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,16 +8,18 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    min_coins = [float('inf')] * (total + 1)
-    # zero coins to return for zero total
-    min_coins[0] = 0
+    coins.sort(reverse=True)
 
+    num_coins = 0
     for coin in coins:
-        for amount in range(coin, total + 1):
-            min_coins[amount] = min(
-                    min_coins[amount], min_coins[amount - coin] + 1)
+        while total >= coin:
+            total -= coin
+            num_coins += 1
 
-    if min_coins[total] != float('inf'):
-        return min_coins[total]
+        if total == 0:
+            break
+
+    if total == 0:
+        return num_coins
     else:
         return -1
