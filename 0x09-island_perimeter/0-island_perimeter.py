cc68ee5135ca1c5d464f3017908e350039aea987
GirumZ <girum.zebe@gmail.com>
2024-06-25 13:59:14 +0300

    [Update]: Handled bounderies

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,15 +5,18 @@
 def island_perimeter(grid):
     """ Function to calculate the perimeter of an island"""
     perimeter = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
             if grid[i][j] == 1:
-                if grid[i - 1][j] != 1:
+                if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
-                if grid[i + 1][j] != 1:
+                if i == rows - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
-                if grid[i][j - 1] != 1:
+                if j == 0 or grid[i][j - 1] == 0:
                     perimeter += 1
-                if grid[i][j + 1] != 1:
+                if j == cols - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
